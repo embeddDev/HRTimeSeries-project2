2818545375f0708ade688893a3e7178fb45871a9
Jón <jonvg09@ru.is>
2014-09-28 14:48:53 +0000

    test2
    test2

--- project2.R
+++ project2.R
@@ -3,7 +3,7 @@
 #course:  Time Series Analysis, T-862-TIMA
 #Date:    /09/2014
 #Students: Daniel Bergmann Sigtryggsson, Lilja Bjorg Gudmundsdottir, Jon Vilberg Georgsson 
-#test
+#test2
 ############################################################################################
 require("forecast")
 data = read.csv("veks.csv")
